3dfad20758ada6de9f0340717bd692e65e61c49b
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-07-03 15:28:19 +0200

    error check fix

--- cli/doSetup.go
+++ cli/doSetup.go
@@ -1,40 +1,17 @@
 package cli
 
 import (
-	"os"
-
 	log "github.com/Sirupsen/logrus"
-	"github.com/bitrise-io/bitrise-cli/bitrise"
 	"github.com/codegangsta/cli"
 )
 
+const (
+	ENVSTORE_PATH_ENV_KEY         string = "ENVMAN_ENVSTORE_PATH"
+	ENVSTORE_PATH                 string = "/Users/godrei/develop/bitrise/bitrise-cli-test/envstore.yml"
+	FORMATTED_OUTPUT_PATH_ENV_KEY string = "BITRISE_STEP_FORMATTED_OUTPUT_FILE_PATH"
+	FORMATTED_OUTPUT_PATH         string = "/Users/godrei/develop/bitrise/bitrise-cli-test/formout.md"
+)
+
 func doSetup(c *cli.Context) {
-	log.Info("Setup")
-
-	// Envman
-	os.Setenv("ENVMAN_ENVSTORE_PATH", "/Users/godrei/develop/bitrise/bitrise-cli-test/help.yml")
-
-	err := bitrise.RunEnvmanInit()
-	if err != nil {
-		log.Errorln("Failed to run init envman")
-		return
-	}
-
-	envs := map[string]string{
-		"BITRISE_STEP_FORMATTED_OUTPUT_FILE_PATH": "/Users/godrei/develop/bitrise/bitrise-cli-test/formout.md",
-	}
-	for key, value := range envs {
-		err := bitrise.RunEnvmanAdd(key, value)
-		if err != nil {
-			log.Errorln("Failed to run envman add")
-			return
-		}
-	}
-
-	// Stepman
-	err = bitrise.RunStepmanSetup()
-	if err != nil {
-		log.Errorln("Failed to run stepman setup")
-		return
-	}
+	log.Info("Setup -- Coming soon!")
 }
