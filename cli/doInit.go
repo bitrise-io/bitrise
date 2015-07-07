2349e1a9c3e4d74e6cf15d6f5df18e781d562993
Viktor Benei <viktor.benei@gmail.com>
2015-07-07 17:48:31 +0200

    exit with code 1 for not-yet-implemented commands

--- cli/doInit.go
+++ cli/doInit.go
@@ -3,8 +3,10 @@ package cli
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
+	"os"
 )
 
 func doInit(c *cli.Context) {
 	log.Info("[BITRISE_CLI] - Init -- Coming soon!")
+	os.Exit(1)
 }
