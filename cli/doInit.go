6c39990f930b777999afcad6b92f35329f922e87
Viktor Benei <viktor.benei@gmail.com>
2015-07-10 18:41:04 +0200

    init now generates a very basic new-style Bitrise Config YML (instead of the old Workflow JSON)

--- cli/doInit.go
+++ cli/doInit.go
@@ -3,46 +3,66 @@ package cli
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 
+	"gopkg.in/yaml.v2"
+
 	log "github.com/Sirupsen/logrus"
-	models "github.com/bitrise-io/bitrise-cli/models/models_0_9_0"
+	models "github.com/bitrise-io/bitrise-cli/models/models_1_0_0"
+	"github.com/bitrise-io/go-pathutil/pathutil"
 	"github.com/bitrise-io/goinp/goinp"
 	"github.com/codegangsta/cli"
 )
 
 func doInit(c *cli.Context) {
 	log.Info("[BITRISE_CLI] - Init -- Work-in-progress!")
+	bitriseConfigFileRelPath := "./bitrise.yml"
 
-	projectSettingsEnvs := []models.InputModel{}
+	if exists, err := pathutil.IsPathExists(bitriseConfigFileRelPath); err != nil {
+		log.Fatalln("Error:", err)
+	} else if exists {
+		ask := fmt.Sprintf("A config file already exists at %s - do you want to overwrite it?", bitriseConfigFileRelPath)
+		if val, err := goinp.AskForBool(ask); err != nil {
+			log.Fatalln("Error:", err)
+		} else if val == false {
+			log.Infoln("Init canceled, existing file won't be overwritten.")
+			os.Exit(0)
+		}
+	}
 
+	projectSettingsEnvs := []models.EnvironmentItemModel{}
 	if val, err := goinp.AskForString("What's the BITRISE_PROJECT_TITLE?"); err != nil {
 		log.Fatalln(err)
 	} else {
 		projectSettingsEnvs = append(projectSettingsEnvs,
-			models.InputModel{MappedTo: "BITRISE_PROJECT_TITLE", Value: val})
+			models.EnvironmentItemModel{"BITRISE_PROJECT_TITLE": val, "is_expand": "no"})
 	}
 	if val, err := goinp.AskForString("What's your primary development branch's name?"); err != nil {
 		log.Fatalln(err)
 	} else {
 		projectSettingsEnvs = append(projectSettingsEnvs,
-			models.InputModel{MappedTo: "BITRISE_DEV_BRANCH", Value: val})
+			models.EnvironmentItemModel{"BITRISE_DEV_BRANCH": val, "is_expand": "no"})
 	}
 
-	workflowModel := models.WorkflowModel{
-		FormatVersion: "0.9.0", // TODO: move this into a project config file!
-		Environments:  projectSettingsEnvs,
-		Steps:         []models.StepModel{},
+	bitriseConf := models.BitriseConfigModel{
+		FormatVersion: "1.0.0", // TODO: move this into a project config file!
+		App: models.AppModel{
+			Environments: projectSettingsEnvs,
+		},
+		Workflows: map[string]models.WorkflowModel{
+			"primary": models.WorkflowModel{},
+		},
 	}
 
-	if err := SaveToFile("./bitrise.json", workflowModel); err != nil {
+	if err := SaveToFile(bitriseConfigFileRelPath, bitriseConf); err != nil {
 		log.Fatalln("Failed to init:", err)
 	}
 	os.Exit(1)
 }
 
 // SaveToFile ...
-func SaveToFile(pth string, workflowModel models.WorkflowModel) error {
+func SaveToFile(pth string, bitriseConf models.BitriseConfigModel) error {
 	if pth == "" {
 		return errors.New("No path provided")
 	}
@@ -57,21 +77,29 @@ func SaveToFile(pth string, workflowModel models.WorkflowModel) error {
 		}
 	}()
 
-	jsonContBytes, err := generateNonFormattedJSON(workflowModel)
+	contBytes, err := generateYAML(bitriseConf)
 	if err != nil {
 		return err
 	}
 
-	if _, err := file.Write(jsonContBytes); err != nil {
+	if _, err := file.Write(contBytes); err != nil {
 		return err
 	}
 	log.Println()
 	log.Infoln("=> Init success!")
 	log.Infoln("File created at path:", pth)
 	log.Infoln("With the content:")
-	log.Infoln(string(jsonContBytes))
+	log.Infoln(string(contBytes))
 
 	return nil
+}
+
+func generateYAML(v interface{}) ([]byte, error) {
+	bytes, err := yaml.Marshal(v)
+	if err != nil {
+		return []byte{}, err
+	}
+	return bytes, nil
 }
 
 func generateNonFormattedJSON(v interface{}) ([]byte, error) {
--- models/models_1_0_0/models.go
+++ models/models_1_0_0/models.go
@@ -3,17 +3,20 @@ package models
 // -------------------
 // --- Models
 
-// InputModel ...
-type InputModel struct {
-	MappedTo          *string   `json:"mapped_to,omitempty" yaml:"mapped_to,omitempty"`
-	Title             *string   `json:"title,omitempty" yaml:"title,omitempty"`
-	Description       *string   `json:"description,omitempty" yaml:"description,omitempty"`
-	Value             *string   `json:"value,omitempty" yaml:"value,omitempty"`
-	ValueOptions      *[]string `json:"value_options,omitempty" yaml:"value_options,omitempty"`
-	IsRequired        *bool     `json:"is_required,omitempty" yaml:"is_required,omitempty"`
-	IsExpand          *bool     `json:"is_expand,omitempty" yaml:"is_expand,omitempty"`
-	IsDontChangeValue *bool     `json:"is_dont_change_value,omitempty" yaml:"is_dont_change_value,omitempty"`
-}
+// // InputModel ...
+// type InputModel struct {
+// 	MappedTo          string    `json:"mapped_to,omitempty" yaml:"mapped_to,omitempty"`
+// 	Title             *string   `json:"title,omitempty" yaml:"title,omitempty"`
+// 	Description       *string   `json:"description,omitempty" yaml:"description,omitempty"`
+// 	Value             string    `json:"value,omitempty" yaml:"value,omitempty"`
+// 	ValueOptions      *[]string `json:"value_options,omitempty" yaml:"value_options,omitempty"`
+// 	IsRequired        *bool     `json:"is_required,omitempty" yaml:"is_required,omitempty"`
+// 	IsExpand          *bool     `json:"is_expand,omitempty" yaml:"is_expand,omitempty"`
+// 	IsDontChangeValue *bool     `json:"is_dont_change_value,omitempty" yaml:"is_dont_change_value,omitempty"`
+// }
+
+// EnvironmentItemModel ...
+type EnvironmentItemModel map[string]string
 
 // OutputModel ...
 type OutputModel struct {
@@ -24,20 +27,20 @@ type OutputModel struct {
 
 // StepModel ...
 type StepModel struct {
-	ID                  string            `json:"id"`
-	StepLibSource       string            `json:"steplib_source"`
-	VersionTag          string            `json:"version_tag"`
-	Name                string            `json:"name" yaml:"name"`
-	Description         *string           `json:"description,omitempty" yaml:"description,omitempty"`
-	Website             string            `json:"website" yaml:"website"`
-	ForkURL             *string           `json:"fork_url,omitempty" yaml:"fork_url,omitempty"`
-	Source              map[string]string `json:"source" yaml:"source"`
-	HostOsTags          *[]string         `json:"host_os_tags,omitempty" yaml:"host_os_tags,omitempty"`
-	ProjectTypeTags     *[]string         `json:"project_type_tags,omitempty" yaml:"project_type_tags,omitempty"`
-	TypeTags            *[]string         `json:"type_tags,omitempty" yaml:"type_tags,omitempty"`
-	IsRequiresAdminUser *bool             `json:"is_requires_admin_user,omitempty" yaml:"is_requires_admin_user,omitempty"`
-	Inputs              []*InputModel     `json:"inputs,omitempty" yaml:"inputs,omitempty"`
-	Outputs             []*OutputModel    `json:"outputs,omitempty" yaml:"outputs,omitempty"`
+	ID                  string                 `json:"id"`
+	StepLibSource       string                 `json:"steplib_source"`
+	VersionTag          string                 `json:"version_tag"`
+	Name                string                 `json:"name" yaml:"name"`
+	Description         *string                `json:"description,omitempty" yaml:"description,omitempty"`
+	Website             string                 `json:"website" yaml:"website"`
+	ForkURL             *string                `json:"fork_url,omitempty" yaml:"fork_url,omitempty"`
+	Source              map[string]string      `json:"source" yaml:"source"`
+	HostOsTags          *[]string              `json:"host_os_tags,omitempty" yaml:"host_os_tags,omitempty"`
+	ProjectTypeTags     *[]string              `json:"project_type_tags,omitempty" yaml:"project_type_tags,omitempty"`
+	TypeTags            *[]string              `json:"type_tags,omitempty" yaml:"type_tags,omitempty"`
+	IsRequiresAdminUser *bool                  `json:"is_requires_admin_user,omitempty" yaml:"is_requires_admin_user,omitempty"`
+	Inputs              []EnvironmentItemModel `json:"inputs,omitempty" yaml:"inputs,omitempty"`
+	Outputs             []*OutputModel         `json:"outputs,omitempty" yaml:"outputs,omitempty"`
 }
 
 // StepGroupModel ...
@@ -59,11 +62,22 @@ type StepCollectionModel struct {
 	DownloadLocations    []map[string]string `json:"download_locations" yaml:"download_locations"`
 }
 
-// WorkFlowModel ...
-type WorkFlowModel struct {
-	FormatVersion string      `json:"format_version"`
-	Environments  []string    `json:"environments"`
-	Steps         []StepModel `json:"steps"`
+// WorkflowModel ...
+type WorkflowModel struct {
+	Environments []string    `json:"environments"`
+	Steps        []StepModel `json:"steps"`
+}
+
+// AppModel ...
+type AppModel struct {
+	Environments []EnvironmentItemModel `json:"environments" yaml:"environments"`
+}
+
+// BitriseConfigModel ...
+type BitriseConfigModel struct {
+	FormatVersion string                   `json:"format_version" yaml:"format_version"`
+	App           AppModel                 `json:"app" yaml:"app"`
+	Workflows     map[string]WorkflowModel `json:"workflows" yaml:"workflows"`
 }
 
 // -------------------
