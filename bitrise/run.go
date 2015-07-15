916e2b3501bb856dc3fab537a1990cd68a3c7e80
Viktor Benei <viktor.benei@gmail.com>
2015-07-15 18:50:08 +0200

    stepman related #fix : the position of the --collection flag changed

--- bitrise/run.go
+++ bitrise/run.go
@@ -14,14 +14,14 @@ import (
 // RunStepmanSetup ...
 func RunStepmanSetup(collection string) error {
 	logLevel := log.GetLevel().String()
-	args := []string{"--debug", "--loglevel", logLevel, "--collection", collection, "setup"}
+	args := []string{"--debug", "--loglevel", logLevel, "setup", "--collection", collection}
 	return RunCommand("stepman", args...)
 }
 
 // RunStepmanActivate ...
 func RunStepmanActivate(collection, stepID, stepVersion, dir string) error {
 	logLevel := log.GetLevel().String()
-	args := []string{"--debug", "--loglevel", logLevel, "--collection", collection, "activate", "--id", stepID, "--version", stepVersion, "--path", dir}
+	args := []string{"--debug", "--loglevel", logLevel, "activate", "--collection", collection, "--id", stepID, "--version", stepVersion, "--path", dir}
 	return RunCommand("stepman", args...)
 }
 
