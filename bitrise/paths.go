e4aab0538fadb3dcd5a5529642c7defa7cacdc09
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-07-16 15:58:10 +0200

    remove file fix

--- bitrise/paths.go
+++ bitrise/paths.go
@@ -46,7 +46,7 @@ func RemoveFile(pth string) error {
 	if exist, err := pathutil.IsPathExists(pth); err != nil {
 		return err
 	} else if exist {
-		if err := os.RemoveAll(pth); err != nil {
+		if err := os.Remove(pth); err != nil {
 			return err
 		}
 	}
