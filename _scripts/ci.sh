710f90fe7a919a3410dcd6f372eeec838b45a8d1
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-08-06 12:27:50 +0200

    fixed ci.sh

--- _scripts/ci.sh
+++ _scripts/ci.sh
@@ -7,13 +7,10 @@ export REPO_ROOT_DIR="${THIS_SCRIPT_DIR}/.."
 
 set -v
 
-bash "${THIS_SCRIPT_DIR}/common/ci.sh"
-
-# TODO:
-#  do a `go build` and run a couple of test commands with it
-
 go build -o tmpbin
 ./tmpbin setup
 rm ./tmpbin
+
+bash "${THIS_SCRIPT_DIR}/common/ci.sh"
 
 # ===> DONE
