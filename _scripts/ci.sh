4de3fe16cb2b9a75eb396b5b9bd31b276bd0b7e1
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-08-06 12:58:17 +0200

    ci.sh fix

--- _scripts/ci.sh
+++ _scripts/ci.sh
@@ -7,6 +7,12 @@ export REPO_ROOT_DIR="${THIS_SCRIPT_DIR}/.."
 
 set -v
 
+# Install dependencies
+go get -u github.com/tools/godep
+go install github.com/tools/godep
+godep restore
+
+# Build a test version
 go build -o tmpbin
 ./tmpbin setup
 rm ./tmpbin
