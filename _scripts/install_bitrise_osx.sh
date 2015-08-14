7f0c2e4d41afed19bb3e4768e6f6bf5943a9b174
Viktor Benei <viktor.benei@gmail.com>
2015-08-14 14:19:09 +0200

    install bitrise cli for ci script

---
+++ _scripts/install_bitrise_osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -v
+
+
+curl -L https://github.com/bitrise-io/bitrise/releases/download/0.9.9/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
+
+chmod +x /usr/local/bin/bitrise
+
+bitrise setup
