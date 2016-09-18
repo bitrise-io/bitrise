5d3e76444cdf9d0308f3ccd0b9be01071c1a587a
Viktor Benei <viktor.benei@gmail.com>
2016-09-18 10:06:48 +0200

    comment format change

--- _tests/integration/invalid_command_test.go
+++ _tests/integration/invalid_command_test.go
@@ -5,8 +5,9 @@ import "testing"
 func Test_InvalidCommand(t *testing.T) {
 	t.Log("Invalid command")
 	{
-		// TODO: there should be an error for invalid command,
-		//  but it won't return any right now; exit code is 0 :/
+		// TODO: there should be an error for invalid command!
+		// Right now it won't return any error; exit code is 0 :/
+
 		// out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "invalidcmd")
 		// require.EqualError(t, err, "")
 		// require.Equal(t, "1.4.0", out)
