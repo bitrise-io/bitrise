2a4ede6f41e1cf02a8dfe545c7e76538dad02f5a
Viktor Benei <viktor.benei@gmail.com>
2016-09-18 09:55:56 +0200

    base for integration tests in go

---
+++ _tests/integration/version_test.go
@@ -0,0 +1,27 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/bitrise-io/go-utils/cmdex"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_VersionOutput(t *testing.T) {
+	t.Log("Version")
+	{
+		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
+		require.NoError(t, err)
+		require.Equal(t, "1.4.0", out)
+	}
+
+	t.Log("Version --full")
+	{
+		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version", "--full")
+		require.NoError(t, err)
+		require.Equal(t, `version: 1.4.0
+format version: 1.3.0
+build number:`+` `+`
+commit:`, out)
+	}
+}
