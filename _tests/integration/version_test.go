4010fbab858135b1bf113e840d51c9822fe61a12
Krisztian Godrei <godreikrisztian@gmail.com>
2016-10-11 15:11:17 +0200

    version test update

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -12,14 +12,14 @@ func Test_VersionOutput(t *testing.T) {
 	{
 		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
 		require.NoError(t, err)
-		require.Equal(t, "1.4.0", out)
+		require.Equal(t, "1.4.1", out)
 	}
 
 	t.Log("Version --full")
 	{
 		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version", "--full")
 		require.NoError(t, err)
-		require.Equal(t, `version: 1.4.0
+		require.Equal(t, `version: 1.4.1
 format version: 1.3.0
 build number:`+` `+`
 commit:`, out)
