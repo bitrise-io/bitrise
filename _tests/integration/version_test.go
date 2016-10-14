5b4f854c5bc2c876394a2f4693c463c017758fbb
Krisztian Godrei <godreikrisztian@gmail.com>
2016-10-14 13:38:32 +0200

    model version: 1.3.1

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -12,15 +12,15 @@ func Test_VersionOutput(t *testing.T) {
 	{
 		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
 		require.NoError(t, err)
-		require.Equal(t, "1.4.1", out)
+		require.Equal(t, "1.4.2", out)
 	}
 
 	t.Log("Version --full")
 	{
 		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version", "--full")
 		require.NoError(t, err)
-		require.Equal(t, `version: 1.4.1
-format version: 1.3.0
+		require.Equal(t, `version: 1.4.2
+format version: 1.3.1
 build number:`+` `+`
 commit:`, out)
 	}
--- models/models.go
+++ models/models.go
@@ -20,7 +20,7 @@ const (
 	StepRunStatusCodeSkippedWithRunIf = 4
 
 	// Version ...
-	Version = "1.3.0"
+	Version = "1.3.1"
 )
 
 // StepListItemModel ...
