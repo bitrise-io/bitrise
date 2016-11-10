742cd0162fc683aa7ffbc0160bdbdbb13c572bca
Krisztian Godrei <godreikrisztian@gmail.com>
2016-11-10 16:38:06 +0100

    prepare for 1.4.5

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -14,7 +14,7 @@ func Test_VersionOutput(t *testing.T) {
 	{
 		out, err := cmdex.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
 		require.NoError(t, err)
-		require.Equal(t, "1.4.4", out)
+		require.Equal(t, "1.4.5", out)
 	}
 
 	t.Log("Version --full")
@@ -23,7 +23,7 @@ func Test_VersionOutput(t *testing.T) {
 		require.NoError(t, err)
 
 		expectedOSVersion := fmt.Sprintf("%s (%s)", runtime.GOOS, runtime.GOARCH)
-		expectedVersionOut := fmt.Sprintf(`version: 1.4.4
+		expectedVersionOut := fmt.Sprintf(`version: 1.4.5
 format version: 1.3.1
 os: %s
 go: %s
