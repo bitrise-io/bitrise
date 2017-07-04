a16863db4528d06ae467e49319dfb38f8c648cf1
Krisztian Godrei <godreikrisztian@gmail.com>
2017-07-04 17:24:21 +0200

    merged

--- cli/setup.go
+++ cli/setup.go
@@ -2,12 +2,37 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/bitrise-io/bitrise/bitrise"
+	"github.com/bitrise-io/bitrise/configs"
+	"github.com/bitrise-io/bitrise/plugins"
 	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/urfave/cli"
 )
+
+var setupCommand = cli.Command{
+	Name:  "setup",
+	Usage: "Setup the current host. Install every required tool to run Workflows.",
+	Action: func(c *cli.Context) error {
+		if err := setup(c); err != nil {
+			log.Errorf("Setup failed, error: %s", err)
+			os.Exit(1)
+		}
+		return nil
+	},
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "full",
+			Usage: "Also calls 'brew doctor'.",
+		},
+		cli.BoolFlag{
+			Name:  "clean",
+			Usage: "Removes bitrise's workdir before setup.",
+		},
+	},
+}
 
 // PrintBitriseHeaderASCIIArt ...
 func PrintBitriseHeaderASCIIArt(appVersion string) {
@@ -27,15 +52,25 @@ func PrintBitriseHeaderASCIIArt(appVersion string) {
 func setup(c *cli.Context) error {
 	PrintBitriseHeaderASCIIArt(c.App.Version)
 
-	if c.IsSet(MinimalModeKey) {
-		log.Warn("'minimal' flag is deprecated")
-		log.Warn("currently setup without any flag does the same as minimal setup in previous versions")
-		log.Warn("use 'full' flag to achive the full setup process (which includes the 'brew doctor' call)")
-		fmt.Println()
+	fullMode := c.Bool("full")
+	cleanMode := c.Bool("clean")
+
+	if cleanMode {
+		if err := configs.DeleteBitriseConfigDir(); err != nil {
+			return err
+		}
+
+		if err := configs.InitPaths(); err != nil {
+			return err
+		}
+
+		if err := plugins.InitPaths(); err != nil {
+			return err
+		}
 	}
 
-	if err := bitrise.RunSetup(c.App.Version, c.Bool(FullModeKey)); err != nil {
-		log.Fatalf("Setup failed, error: %s", err)
+	if err := bitrise.RunSetup(c.App.Version, fullMode); err != nil {
+		return err
 	}
 
 	log.Infoln("To start using bitrise:")
