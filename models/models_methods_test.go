d4537077a52473e1329b8f75a2a684feeb88514e
Krisztian Godrei <godreikrisztian@gmail.com>
2017-08-08 11:17:39 +0200

    code cleanup

--- models/models_methods_test.go
+++ models/models_methods_test.go
@@ -1391,23 +1391,6 @@ func TestCreateStepIDDataFromString(t *testing.T) {
 // ----------------------------
 // --- RemoveRedundantFields
 
-/*
-	// These fields are processed by envman at envman run
-	IsExpand    *bool `json:"is_expand,omitempty" yaml:"is_expand,omitempty"`
-	SkipIfEmpty *bool `json:"skip_if_empty,omitempty" yaml:"skip_if_empty,omitempty"`
-	// These fields used only by bitrise
-	Title             *string  `json:"title,omitempty" yaml:"title,omitempty"`
-	Description       *string  `json:"description,omitempty" yaml:"description,omitempty"`
-	Summary           *string  `json:"summary,omitempty" yaml:"summary,omitempty"`
-	Category          *string  `json:"category,omitempty" yaml:"category,omitempty"`
-	ValueOptions      []string `json:"value_options,omitempty" yaml:"value_options,omitempty"`
-	IsRequired        *bool    `json:"is_required,omitempty" yaml:"is_required,omitempty"`
-	IsDontChangeValue *bool    `json:"is_dont_change_value,omitempty" yaml:"is_dont_change_value,omitempty"`
-	IsTemplate        *bool    `json:"is_template,omitempty" yaml:"is_template,omitempty"`
-	//
-	Meta map[string]interface{} `json:"meta,omitempty" yaml:"meta,omitempty"`
-*/
-
 func TestRemoveEnvironmentRedundantFields(t *testing.T) {
 	t.Log("Trivial remove - all fields should be default value")
 	{
