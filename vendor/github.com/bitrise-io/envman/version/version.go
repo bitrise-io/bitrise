74430590c8537a36a316be53b7fec16c72fb13ac
Krisztián Gödrei <godreikrisztian@gmail.com>
2018-05-08 11:40:27 +0200

    dep update (#599)

--- vendor/github.com/bitrise-io/envman/version/version.go
+++ vendor/github.com/bitrise-io/envman/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "1.1.12"
+const VERSION = "1.1.13"
--- vendor/github.com/bitrise-io/stepman/_tests/integration/version_test.go
+++ vendor/github.com/bitrise-io/stepman/_tests/integration/version_test.go
@@ -14,7 +14,7 @@ func Test_VersionOutput(t *testing.T) {
 	{
 		out, err := command.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
 		require.NoError(t, err, out)
-		require.Equal(t, "0.9.40", out)
+		require.Equal(t, "0.9.41", out)
 	}
 
 	t.Log("Version --full")
@@ -23,7 +23,7 @@ func Test_VersionOutput(t *testing.T) {
 		require.NoError(t, err, out)
 
 		expectedOSVersion := fmt.Sprintf("%s (%s)", runtime.GOOS, runtime.GOARCH)
-		expectedVersionOut := fmt.Sprintf(`version: 0.9.40
+		expectedVersionOut := fmt.Sprintf(`version: 0.9.41
 os: %s
 go: %s
 build_number: 
