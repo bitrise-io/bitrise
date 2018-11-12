32e0e5b7619f4af8379a0845405ebe0a77b834f0
Akos Birmacher <akos.birmacher@bitrise.io>
2018-11-12 15:32:38 +0100

    dep update, tools and plugins update, version bump to: 1.24.0

--- vendor/github.com/bitrise-io/envman/version/version.go
+++ vendor/github.com/bitrise-io/envman/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "2.1.0"
+const VERSION = "2.1.1"
--- vendor/github.com/bitrise-io/stepman/version/version.go
+++ vendor/github.com/bitrise-io/stepman/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "0.10.4"
+const VERSION = "0.10.5"
