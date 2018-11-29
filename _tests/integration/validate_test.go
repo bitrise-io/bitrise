4fe3a8c3f8a7fce420003a74790a6ce16d1c865a
Laszlo Szucs <laszlo.szucs@bitrise.io>
2018-11-29 16:32:44 +0100

    add runing time check to tests

--- _tests/integration/validate_test.go
+++ _tests/integration/validate_test.go
@@ -23,18 +23,28 @@ func Test_ValidateTest(t *testing.T) {
 
 	t.Log("valid bitrise.yml")
 	{
-		cmd := command.New(binPath(), "validate", "-c", "trigger_params_test_bitrise.yml")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", "trigger_params_test_bitrise.yml")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		require.Equal(t, "Config is valid: \x1b[32;1mtrue\x1b[0m", out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("valid - warning test - `-p` flag is deprecated")
 	{
-		cmd := command.New(binPath(), "validate", "-p", "trigger_params_test_bitrise.yml")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-p", "trigger_params_test_bitrise.yml")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		require.Equal(t, "Config is valid: \x1b[32;1mtrue\x1b[0m\nWarning(s):\n- 'path' key is deprecated, use 'config' instead!", out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("valid - invalid workflow id")
@@ -42,11 +52,16 @@ func Test_ValidateTest(t *testing.T) {
 		configPth := filepath.Join(tmpDir, "bitrise.yml")
 		require.NoError(t, fileutil.WriteStringToFile(configPth, invalidWorkflowIDBitriseYML))
 
-		cmd := command.New(binPath(), "validate", "-c", configPth)
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", configPth)
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		expected := "Config is valid: \x1b[32;1mtrue\x1b[0m\nWarning(s):\n- invalid workflow ID (invalid:id): doesn't conform to: [A-Za-z0-9-_.]"
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("invalid - empty bitrise.yml")
@@ -54,11 +69,16 @@ func Test_ValidateTest(t *testing.T) {
 		configPth := filepath.Join(tmpDir, "bitrise.yml")
 		require.NoError(t, fileutil.WriteStringToFile(configPth, emptyBitriseYML))
 
-		cmd := command.New(binPath(), "validate", "-c", configPth)
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", configPth)
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.Error(t, err, out)
 		expected := fmt.Sprintf("Config is valid: \x1b[31;1mfalse\x1b[0m\nError: \x1b[31;1mConfig (path:%s) is not valid: empty config\x1b[0m", configPth)
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 }
 
@@ -71,18 +91,28 @@ func Test_ValidateTestJSON(t *testing.T) {
 
 	t.Log("valid bitrise.yml")
 	{
-		cmd := command.New(binPath(), "validate", "-c", "trigger_params_test_bitrise.yml", "--format", "json")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", "trigger_params_test_bitrise.yml", "--format", "json")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		require.Equal(t, "{\"data\":{\"config\":{\"is_valid\":true}}}", out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("valid - warning test - `-p` flag is deprecated")
 	{
-		cmd := command.New(binPath(), "validate", "-p", "trigger_params_test_bitrise.yml", "--format", "json")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-p", "trigger_params_test_bitrise.yml", "--format", "json")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		require.Equal(t, "{\"data\":{\"config\":{\"is_valid\":true}},\"warnings\":[\"'path' key is deprecated, use 'config' instead!\"]}", out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("valid - invalid workflow id")
@@ -90,11 +120,16 @@ func Test_ValidateTestJSON(t *testing.T) {
 		configPth := filepath.Join(tmpDir, "bitrise.yml")
 		require.NoError(t, fileutil.WriteStringToFile(configPth, invalidWorkflowIDBitriseYML))
 
-		cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		expected := "{\"data\":{\"config\":{\"is_valid\":true,\"warnings\":[\"invalid workflow ID (invalid:id): doesn't conform to: [A-Za-z0-9-_.]\"]}}}"
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("invalid - empty bitrise.yml")
@@ -102,11 +137,16 @@ func Test_ValidateTestJSON(t *testing.T) {
 		configPth := filepath.Join(tmpDir, "bitrise.yml")
 		require.NoError(t, fileutil.WriteStringToFile(configPth, emptyBitriseYML))
 
-		cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.Error(t, err, out)
 		expected := fmt.Sprintf("{\"data\":{\"config\":{\"is_valid\":false,\"error\":\"Config (path:%s) is not valid: empty config\"}}}", configPth)
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("invalid - only one space in bitrise.yml")
@@ -114,11 +154,16 @@ func Test_ValidateTestJSON(t *testing.T) {
 		configPth := filepath.Join(tmpDir, "bitrise.yml")
 		require.NoError(t, fileutil.WriteStringToFile(configPth, spaceBitriseYML))
 
-		cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-c", configPth, "--format", "json")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.Error(t, err, out)
 		expected := fmt.Sprintf("{\"data\":{\"config\":{\"is_valid\":false,\"error\":\"Config (path:%s) is not valid: missing format_version\"}}}", configPth)
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 }
 
@@ -131,10 +176,15 @@ func Test_SecretValidateTest(t *testing.T) {
 
 	t.Log("valid secret")
 	{
-		cmd := command.New(binPath(), "validate", "-i", "global_flag_test_secrets.yml")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-i", "global_flag_test_secrets.yml")
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.NoError(t, err)
 		require.Equal(t, "Secret is valid: \x1b[32;1mtrue\x1b[0m", out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("invalid - empty secret")
@@ -142,11 +192,16 @@ func Test_SecretValidateTest(t *testing.T) {
 		secretsPth := filepath.Join(tmpDir, "secrets.yml")
 		require.NoError(t, fileutil.WriteStringToFile(secretsPth, emptySecret))
 
-		cmd := command.New(binPath(), "validate", "-i", secretsPth)
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-i", secretsPth)
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.Error(t, err, out)
 		expected := "Secret is valid: \x1b[31;1mfalse\x1b[0m\nError: \x1b[31;1mempty config\x1b[0m"
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 
 	t.Log("invalid - invalid secret model")
@@ -154,11 +209,16 @@ func Test_SecretValidateTest(t *testing.T) {
 		secretsPth := filepath.Join(tmpDir, "secrets.yml")
 		require.NoError(t, fileutil.WriteStringToFile(secretsPth, invalidSecret))
 
-		cmd := command.New(binPath(), "validate", "-i", secretsPth)
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		var out string
+		var err error
+		elapsed := withRunningTimeCheck(func() {
+			cmd := command.New(binPath(), "validate", "-i", secretsPth)
+			out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+		}, runtimeLimit)
 		require.Error(t, err, out)
 		expected := "Secret is valid: \x1b[31;1mfalse\x1b[0m\nError: \x1b[31;1mInvalid inventory format: yaml: unmarshal errors:\n  line 1: cannot unmarshal !!seq into models.EnvsSerializeModel\x1b[0m"
 		require.Equal(t, expected, out)
+		assertRunTime(t, elapsed, runtimeLimit)
 	}
 }
 
