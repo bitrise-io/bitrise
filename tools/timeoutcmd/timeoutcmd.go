3604a5b08d504a71ba195d135283838a0c09c304
Papik Tamas <trapacska@gmail.com>
2019-02-25 14:31:50 +0100

    clean-up

--- tools/timeoutcmd/timeoutcmd.go
+++ tools/timeoutcmd/timeoutcmd.go
@@ -21,11 +21,10 @@ type Command struct {
 
 // New creates a command model.
 func New(dir, name string, args ...string) Command {
-	c := Command{}
-
-	c.cmd = exec.Command(name, args...)
+	c := Command{
+		cmd: exec.Command(name, args...),
+	}
 	c.cmd.Dir = dir
-
 	return c
 }
 
@@ -45,9 +44,7 @@ func (c *Command) AppendEnv(env string) {
 
 // SetStandardIO sets the input and outputs of the command.
 func (c *Command) SetStandardIO(in io.Reader, out, err io.Writer) {
-	c.cmd.Stdin = in
-	c.cmd.Stdout = out
-	c.cmd.Stderr = err
+	c.cmd.Stdin, c.cmd.Stdout, c.cmd.Stderr = in, out, err
 }
 
 // Start starts the command run.
@@ -56,8 +53,8 @@ func (c *Command) Start() error {
 	// this way cmd will not belong to its parent process group,
 	// cmd will not be killed when you hit ^C in your terminal
 	// to fix this, we listen and handle Interrupt signal manually
-	c.interruptChan = make(chan os.Signal, 1)
-	signal.Notify(c.interruptChan, os.Interrupt)
+	c.interruptChan = make(chan os.Signal)
+	signal.Notify(c.interruptChan, os.Interrupt, os.Kill)
 	c.cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 
 	if err := c.cmd.Start(); err != nil {
@@ -67,18 +64,22 @@ func (c *Command) Start() error {
 	if c.timeout > 0 {
 		// terminate the process after the given timeout
 		c.timeoutTimer = time.AfterFunc(c.timeout, func() {
-			if err := c.Stop(syscall.SIGTERM); err != nil {
-				log.Warnf("Failed to kill the process, error: %s", err)
+			if err := c.Stop(os.Kill); err != nil {
+				log.Warnf("Failed to kill process, error: %s", err)
 			}
 		})
 	}
 
 	var interrupted bool
+
 	go func() {
-		<-c.interruptChan
-		interrupted = true
-		if err := c.Stop(syscall.SIGINT); err != nil {
-			log.Warnf("Failed to kill the process, error: %s", err)
+		for {
+			sig := <-c.interruptChan
+			interrupted = true
+
+			if err := c.Stop(sig); err != nil {
+				log.Warnf("Failed to stop process, error: %s", err)
+			}
 		}
 	}()
 
@@ -92,23 +93,26 @@ func (c *Command) Start() error {
 }
 
 // Stop terminates the command run.
-func (c *Command) Stop(sig syscall.Signal) error {
+func (c *Command) Stop(sig os.Signal) error {
 	if c.cmd.Process == nil {
-		// not yet started
+		// not yet started or there was an error starting the command
 		return nil
 	}
 
 	if c.timeout > 0 {
-		// stop the timeout timer
+		// stop the timeout timer if it was set
 		c.timeoutTimer.Stop()
 	}
 
-	pgid, err := syscall.Getpgid(c.cmd.Process.Pid)
-	if err != nil {
-		return err
+	signum := syscall.SIGKILL
+	if sig == os.Interrupt {
+		signum = syscall.SIGINT
 	}
 
-	return syscall.Kill(-pgid, sig)
+	// all child processes will be included in the current process' group and
+	// this group ID will be the original PID of the child because of Setpgid
+	// - is to kill the whole group not the only children
+	return syscall.Kill(-c.cmd.Process.Pid, signum)
 }
 
 // ExitStatus returns the error's exit status
