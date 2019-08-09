3c52b92a8595fcdfbbc9d5406bfc3cbf4070ea07
Richard Bogdan <richard.bogdan@bitrise.io>
2019-08-09 15:30:27 +0200

    TOOL-667 Moved tests.

--- _tests/integration/async_step_test.go
+++ _tests/integration/async_step_test.go
@@ -10,9 +10,8 @@ import (
 func Test_AsyncStep(t *testing.T) {
 	configPth := "async_step_test_bitrise.yml"
 
-	for _, aTestWFID := range []string{
-		"asynctest",
-	} {
+	aTestWFID := "asynctest"
+	{
 		t.Log(aTestWFID)
 		{
 			cmd := command.New(binPath(), "run", aTestWFID, "--config", configPth)
