62978c55dd201ecc250c49cc6a591de6d20e1c7e
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-08-13 13:52:29 +0200

    update go dependencies

--- vendor/github.com/bitrise-io/go-utils/envutil/envutil.go
+++ vendor/github.com/bitrise-io/go-utils/envutil/envutil.go
@@ -1,6 +1,9 @@
 package envutil
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // RevokableSetenv ...
 func RevokableSetenv(envKey, envValue string) (func() error, error) {
@@ -40,4 +43,31 @@ func GetenvWithDefault(envKey, defValue string) string {
 		return defValue
 	}
 	return retVal
+}
+
+// RequiredEnv - returns the env's value if specified, otherwise it returns an error that explains the key is required.
+// Use this function to reduce error prone code duplication.
+// E.g. instead of doing this in your code:
+//
+// 	myVar1 := os.Getenv("MY_ENV1")
+// 	if len(myVar1) < 1 {
+// 		return nil, errors.New("MY_ENV1 required")
+// 	}
+//
+// You can use this function like:
+//
+// 	myVar1, err := requiredEnv("MY_ENV1")
+// 	if err != nil {
+// 		return nil, errors.WithStack(err)
+// 	}
+//
+// In the first example you have to specify myVar1 and MY_ENV1 two times, which can lead to
+// issues if you copy-paste that code but e.g. forget to change the var name in the
+// 	if len(myVar1) < 1
+// line, or if you forget to change the var key in the error message/string.
+func RequiredEnv(envKey string) (string, error) {
+	if val := os.Getenv(envKey); len(val) > 0 {
+		return val, nil
+	}
+	return "", fmt.Errorf("required environment variable (%s) not provided", envKey)
 }
