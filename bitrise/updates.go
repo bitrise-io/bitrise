9673341e27e0efb4493e39ab43c6785cd54423d9
Laszlo Szucs <laszlo.szucs@bitrise.io>
2020-01-20 11:09:11 +0100

    refactor: consolidate update check flow

--- bitrise/updates.go
+++ bitrise/updates.go
@@ -4,8 +4,6 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-utils/versions"
 	stepmanModels "github.com/bitrise-io/stepman/models"
 	ver "github.com/hashicorp/go-version"
 )
@@ -15,26 +13,20 @@ func isUpdateAvailable(stepInfo stepmanModels.StepInfoModel) bool {
 		return false
 	}
 
-	if stepInfo.Version != stepInfo.EvaluatedVersion {
-		re := regexp.MustCompile(`\d+`)
-		components := re.FindAllString(stepInfo.Version, -1)
-		normalized := strings.Join(components, ".")
-		locked, _ := ver.NewSemver(normalized)
-		latest, _ := ver.NewSemver(stepInfo.LatestVersion)
+	re := regexp.MustCompile(`\d+`)
+	components := re.FindAllString(stepInfo.Version, -1)
+	normalized := strings.Join(components, ".")
+	locked, _ := ver.NewSemver(normalized)
+	latest, _ := ver.NewSemver(stepInfo.LatestVersion)
 
-		switch len(components) {
-		case 1:
-			return locked.Segments()[0] < latest.Segments()[0]
-		case 2:
-			return locked.Segments()[0] < latest.Segments()[0] || locked.Segments()[1] < latest.Segments()[1]
-
-		}
+	switch len(components) {
+	case 1:
+		return locked.Segments()[0] < latest.Segments()[0]
+	case 2:
+		return locked.Segments()[0] < latest.Segments()[0] || locked.Segments()[1] < latest.Segments()[1]
+	case 3:
+		return locked.LessThan(latest)
+	default:
+		return false
 	}
-
-	res, err := versions.CompareVersions(stepInfo.Version, stepInfo.LatestVersion)
-	if err != nil {
-		log.Errorf("Failed to compare versions, err: %s", err)
-	}
-
-	return (res == 1)
 }
