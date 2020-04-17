5358975f8c090ade0371f64debeffd99dcacb2b8
Áron Szabados <aronszabados@gmail.com>
2020-04-17 12:48:40 +0200

    require new plugin, version bump

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -14,7 +14,7 @@ func Test_VersionOutput(t *testing.T) {
 	{
 		out, err := command.RunCommandAndReturnCombinedStdoutAndStderr(binPath(), "version")
 		require.NoError(t, err)
-		require.Equal(t, "1.41.0", out)
+		require.Equal(t, "1.41.1", out)
 	}
 
 	t.Log("Version --full")
@@ -23,7 +23,7 @@ func Test_VersionOutput(t *testing.T) {
 		require.NoError(t, err)
 
 		expectedOSVersion := fmt.Sprintf("%s (%s)", runtime.GOOS, runtime.GOARCH)
-		expectedVersionOut := fmt.Sprintf(`version: 1.41.0
+		expectedVersionOut := fmt.Sprintf(`version: 1.41.1
 format version: 10
 os: %s
 go: %s
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "1.41.0"
+const VERSION = "1.41.1"
