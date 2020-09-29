545fb733ce7eba325bbda1d041a2ed3e53820ffd
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2020-09-29 16:22:57 +0200

    Release 1.43.0 (#734)
    * Depend on stepman 0.13.0
    
    * Bump version to 1.43.0
    
    * Dep update

--- vendor/github.com/bitrise-io/stepman/cli/share_finish.go
+++ vendor/github.com/bitrise-io/stepman/cli/share_finish.go
@@ -2,10 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/bitrise-io/colorstring"
 	"github.com/bitrise-io/go-utils/command/git"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/stepman/stepman"
 	"github.com/urfave/cli"
 )
@@ -17,6 +21,22 @@ func printFinishShare() {
 	b.Plain("On GitHub you can find a ").Blue("Compare & pull request").Plain(" button, in the section called ").Blue("Your recently pushed branches:").Plain(",").NewLine()
 	b.Plain("which will bring you to the page to ").Blue("Open a pull request").Plain(", where you can review and create your Pull Request.")
 	fmt.Println(b.String())
+}
+
+func addStepGroupSpecIfExists(route stepman.SteplibRoute, stepID, gitstatus string, repo git.Git) error {
+	stepInfoYMLPathInSteplib := stepman.GetStepGlobalInfoPath(route, stepID)
+	if exists, err := pathutil.IsPathExists(stepInfoYMLPathInSteplib); err == nil {
+		if exists && strings.Contains(gitstatus, path.Base(stepInfoYMLPathInSteplib)) {
+			log.Printf("new step-info.yml: %s", stepInfoYMLPathInSteplib)
+			if err := repo.Add(stepInfoYMLPathInSteplib).Run(); err != nil {
+				return fmt.Errorf("add step-info.yml: %w", err)
+			}
+		}
+	} else {
+		return fmt.Errorf("add step-info.yml: %w", err)
+	}
+
+	return nil
 }
 
 func finish(c *cli.Context) error {
@@ -43,11 +63,11 @@ func finish(c *cli.Context) error {
 		fail(err.Error())
 	}
 
-	out, err := repo.Status("--porcelain").RunAndReturnTrimmedCombinedOutput()
+	gitstatus, err := repo.Status("-u", "--porcelain").RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
 		fail(err.Error())
 	}
-	if out == "" {
+	if gitstatus == "" {
 		log.Warnf("No git changes, it seems you already called this command")
 		fmt.Println()
 		printFinishShare()
@@ -55,9 +75,13 @@ func finish(c *cli.Context) error {
 	}
 
 	stepDirInSteplib := stepman.GetStepCollectionDirPath(route, share.StepID, share.StepTag)
-	stepYMLPathInSteplib := stepDirInSteplib + "/step.yml"
+	stepYMLPathInSteplib := filepath.Join(stepDirInSteplib, "step.yml")
 	log.Printf("new step.yml: %s", stepYMLPathInSteplib)
 	if err := repo.Add(stepYMLPathInSteplib).Run(); err != nil {
+		fail(err.Error())
+	}
+	// add auto generated step-info.yml for new steps
+	if err := addStepGroupSpecIfExists(route, share.StepID, gitstatus, repo); err != nil {
 		fail(err.Error())
 	}
 
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "1.42.0"
+const VERSION = "1.43.0"
