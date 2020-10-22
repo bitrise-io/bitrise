c40d81267dcab198106718dc85f148a8ecbb5d35
József Erős <jozsef.eros@bitrise.io>
2020-10-22 15:29:38 +0200

    Fix failing integration tests

--- _tests/integration/trigger_params_test.go
+++ _tests/integration/trigger_params_test.go
@@ -53,8 +53,8 @@ func Test_TriggerParams(t *testing.T) {
 	t.Log("deprecated pipeline trigger with pattern")
 	{
 		cmd := command.New(binPath(), "trigger", "--pattern", "deprecated_pipeline_trigger", "--config", "trigger_params_test_bitrise.yml")
-		_, err := cmd.RunAndReturnTrimmedCombinedOutput()
-		require.EqualError(t, err, "No workflow id specified")
+		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		require.Error(t, err, out)
 	}
 
 	t.Log("pipeline trigger with push-branch")
