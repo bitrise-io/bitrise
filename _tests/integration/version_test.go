a8383f91876e3364eba1a4e463612575e22ffff8
Imre Kelényi <imre.kelenyi@gmail.com>
2020-12-03 13:13:54 +0100

    bump CLI version

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	expectedCLIVersion = "1.44.0"
+	expectedCLIVersion = "1.44.1"
 )
 
 func Test_VersionOutput(t *testing.T) {
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "1.44.0"
+const VERSION = "1.44.1"
