f3e4c9257c1612dce8f050c95e4d8486a4d753b3
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-03-31 15:02:06 +0200

    Version Bump

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	expectedCLIVersion = "1.45.1"
+	expectedCLIVersion = "1.45.2"
 )
 
 func Test_VersionOutput(t *testing.T) {
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // VERSION ...
-const VERSION = "1.45.1"
+const VERSION = "1.45.2"
