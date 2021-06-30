4d18d3955f70086b5b72be670ababbd00abbe2fb
Tamas Papik <trapacska@gmail.com>
2021-06-30 15:01:41 +0200

    updated envman version and added test for 256Kb env size test

---
+++ _tests/integration/env_size_test.go
@@ -0,0 +1,19 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_EnvstoreTest(t *testing.T) {
+	configPth := "env_size_test_bitrise.yml"
+
+	t.Log("exit_code_test_fail")
+	{
+		cmd := command.New(binPath(), "run", "env-size-test", "--config", configPth)
+		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
+		require.NoError(t, err, out)
+	}
+}
