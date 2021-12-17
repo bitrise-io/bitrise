5f2aa8e7885a7b8070c72d60281b185241f4679f
Olivér Falvai <oliver.falvai@bitrise.io>
2021-12-17 13:58:32 +0100

    Listen for connections in a goroutine, use env var for condition

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
 
 	"github.com/bitrise-io/bitrise/cli"
 )
 
 func main() {
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		print(err.Error())
+	value, ok := os.LookupEnv("ENABLE_PROFILING")
+	if ok && value == "1" {
+		log.Println("pprof enabled over HTTP")
+		go func() {
+			log.Println(http.ListenAndServe("localhost:8080", nil))
+		}()
 	}
 
 	cli.Run()
