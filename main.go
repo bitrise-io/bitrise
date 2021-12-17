469db1486871e9a519d36ac5e4b93484c6420274
Olivér Falvai <oliver.falvai@bitrise.io>
2021-12-17 12:57:37 +0100

    Handle error properly

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main() {
-	_ = http.ListenAndServe("localhost:8080", nil)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		print(err.Error())
+	}
 
 	cli.Run()
 }
