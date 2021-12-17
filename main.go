5aab579c575261b038893388585f38a63d090586
Olivér Falvai <oliver.falvai@bitrise.io>
2021-12-17 12:49:27 +0100

    Add profiling over HTTP

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/bitrise-io/bitrise/cli"
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/bitrise-io/bitrise/cli"
+)
 
 func main() {
+	http.ListenAndServe("localhost:8080", nil)
+
 	cli.Run()
 }
