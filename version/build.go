34d7fa63029912446aa2a780b3f4eff8de3e9c0e
Olivér Falvai <oliver.falvai@bitrise.io>
2022-07-27 11:48:06 +0200

    Define dev version by default, fix tests

--- version/build.go
+++ version/build.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION is the main CLI version number. It's defined at build time using -ldflags
-var VERSION = ""
+var VERSION = "0.0.0-development"
 
 // BuildNumber is the CI build number that creates the release. It's defined at build time using -ldflags
 var BuildNumber = ""
