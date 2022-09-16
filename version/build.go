507e52ec3a64aa3e09f83a8f99f9a2a4cd73c2d4
Szabolcs Toth <szabolcs.toth@bitrise.io>
2022-09-16 14:39:49 +0100

    Add temp version number

--- version/build.go
+++ version/build.go
@@ -1,7 +1,8 @@
 package version
 
+//todo: Remove this version number change
 // VERSION is the main CLI version number. It's defined at build time using -ldflags
-var VERSION = "1.99.0-development"
+var VERSION = "1.99.0-development-json-test"
 
 // BuildNumber is the CI build number that creates the release. It's defined at build time using -ldflags
 var BuildNumber = ""
