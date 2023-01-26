eb941630873e3184774e6382b51885eb8acdc75e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-01-26 15:11:56 +0100

    stop gows command printing to the stdout directly (#847)
    * stop gows command printing to the stdout directly
    
    * Update golang_test.go

--- toolkits/command_runner.go
+++ toolkits/command_runner.go
@@ -10,27 +10,11 @@ import (
 
 // commandRunner ...
 type commandRunner interface {
-	run(c *command.Model) error
 	runForOutput(c *command.Model) (string, error)
 }
 
 // defaultRunner ...
 type defaultRunner struct {
-}
-
-// run ...
-func (r *defaultRunner) run(c *command.Model) error {
-	log.Debugf("$ %s", c.PrintableCommandArgs())
-
-	if err := c.Run(); err != nil {
-		if errorutil.IsExitStatusError(err) {
-			return fmt.Errorf("command `%s` failed: %v", c.PrintableCommandArgs(), err)
-		}
-
-		return fmt.Errorf("failed to run command `%s`: %v", c.PrintableCommandArgs(), err)
-	}
-
-	return nil
 }
 
 // runForOutput ...
