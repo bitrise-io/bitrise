442ce47c3eb1a7cd77126897ee6afbc2e881a7e2
Szabolcs Toth <szabolcs.toth@bitrise.io>
2023-02-08 14:05:17 +0000

    Add versioning logic

--- version/build.go
+++ version/build.go
@@ -1,5 +1,10 @@
 package version
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 // Version is the main CLI version number. It's defined at build time using -ldflags
 var Version = ""
 
@@ -8,3 +13,17 @@ var BuildNumber = ""
 
 // Commit is the git commit hash used for building the release. It's defined at build time using -ldflags
 var Commit = ""
+
+func init() {
+	if Version != "" {
+		return
+	}
+
+	info, available := debug.ReadBuildInfo()
+	if available && info.Main.Version != "" && info.Main.Sum != "" {
+		Version = info.Main.Version
+		Commit = fmt.Sprintf("%q (mod checksum)", info.Main.Sum)
+	} else {
+		Version = "local"
+	}
+}
