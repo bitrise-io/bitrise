578f02fc7cafaf4778bf2ac5eaf4e21583826f9c
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-06-22 10:06:27 +0200

    Unit test secret redaction

--- stepruncmd/stdout_test.go
+++ stepruncmd/stdout_test.go
@@ -58,6 +58,56 @@ func Test_GivenWriter_WhenConsoleLogging_ThenTransmitsLogs(t *testing.T) {
 	}
 }
 
+func Test_GivenWriter_WhenConsoleLoggingAndSecretFiltering_ThenRedactsSecrets(t *testing.T) {
+	tests := []struct {
+		name        string
+		secrets     []string
+		messages    []string
+		wantMessage string
+		wantError   string
+	}{
+		{
+			name:        "Simple message with error",
+			secrets:     []string{"1234"},
+			messages:    []string{`[31;1mInvalid password: 1234[0m`},
+			wantMessage: `[31;1mInvalid password: [REDACTED][0m`,
+			wantError:   "Invalid password: [REDACTED]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buff bytes.Buffer
+			opts := log.LoggerOpts{
+				LoggerType: log.ConsoleLogger,
+				Producer:   "Test",
+				ProducerID: "UUID",
+				Writer:     &buff,
+				TimeProvider: func() time.Time {
+					// UnixNano() is 0 for this time
+					return time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+				},
+			}
+			logger := log.NewLogger(opts)
+			writer := logwriter.NewLogWriter(logger)
+
+			w := NewStdoutWriter(tt.secrets, writer)
+			for _, message := range tt.messages {
+				gotN, err := w.Write([]byte(message))
+				require.NoError(t, err)
+				require.Equal(t, len(message), gotN)
+			}
+
+			err := w.Close()
+			require.NoError(t, err)
+			require.Equal(t, tt.wantMessage, buff.String())
+
+			errors := w.ErrorMessages()
+			require.Equal(t, 1, len(errors))
+			require.Equal(t, tt.wantError, errors[0])
+		})
+	}
+}
+
 func Test_GivenWriter_WhenJSONLogging_ThenWritesJSON(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -220,6 +270,56 @@ func Test_GivenWriter_WhenJSONLoggingAndSecretFiltering_ThenReturnsError(t *test
 
 			errors := w.ErrorMessages()
 			require.Equal(t, tt.want, errors)
+		})
+	}
+}
+
+func Test_GivenWriter_WhenJSONLoggingAndSecretFiltering_ThenRedactsSecrets(t *testing.T) {
+	tests := []struct {
+		name        string
+		secrets     []string
+		messages    []string
+		wantMessage string
+		wantError   string
+	}{
+		{
+			name:        "Simple message with error",
+			secrets:     []string{"1234"},
+			messages:    []string{`[31;1mInvalid password: 1234[0m`},
+			wantMessage: `{"timestamp":"1970-01-01T00:00:00Z","type":"log","producer":"Test","producer_id":"UUID","level":"error","message":"Invalid password: [REDACTED]"}` + "\n",
+			wantError:   "Invalid password: [REDACTED]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buff bytes.Buffer
+			opts := log.LoggerOpts{
+				LoggerType: log.JSONLogger,
+				Producer:   "Test",
+				ProducerID: "UUID",
+				Writer:     &buff,
+				TimeProvider: func() time.Time {
+					// UnixNano() is 0 for this time
+					return time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+				},
+			}
+			logger := log.NewLogger(opts)
+			writer := logwriter.NewLogWriter(logger)
+
+			w := NewStdoutWriter(tt.secrets, writer)
+			for _, message := range tt.messages {
+				gotN, err := w.Write([]byte(message))
+				require.NoError(t, err)
+				require.Equal(t, len(message), gotN)
+			}
+
+			err := w.Close()
+			require.NoError(t, err)
+			require.Equal(t, tt.wantMessage, buff.String())
+
+			errors := w.ErrorMessages()
+			require.Equal(t, 1, len(errors))
+			require.Equal(t, tt.wantError, errors[0])
 		})
 	}
 }
