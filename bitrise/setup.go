7e2e2892dbd2063739f7b1802e49e98a54bb1991
Szabolcs Toth <szabolcs.toth@bitrise.io>
2023-09-20 13:36:04 +0100

    Update version numbers

--- bitrise/setup.go
+++ bitrise/setup.go
@@ -36,7 +36,7 @@ var PluginDependencyMap = map[string]PluginDependency{
 	},
 	"workflow-editor": {
 		Source:     "https://github.com/bitrise-io/bitrise-workflow-editor.git",
-		MinVersion: "1.3.241",
+		MinVersion: "1.3.246",
 	},
 	"analytics": {
 		Source:     "https://github.com/bitrise-io/bitrise-plugins-analytics.git",
