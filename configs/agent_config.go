a1fc541998d304adb8602d7ea31970d740186ccc
Olivér Falvai <ofalvai@gmail.com>
2023-11-10 14:25:12 +0100

    Simplify minimal config

--- configs/agent_config.go
+++ configs/agent_config.go
@@ -3,10 +3,15 @@ package configs
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/bitrise-io/go-utils/pathutil"
 	"gopkg.in/yaml.v2"
 )
+
+const defaultSourceDir = "workspace"
+const defaultDeployDir = "$BITRISE_APP_SLUG/$BITRISE_BUILD_SLUG/artifacts"
+const defaultTestDeployDir = "$BITRISE_APP_SLUG/$BITRISE_BUILD_SLUG/test_results"
 
 type AgentConfig struct {
 	BitriseDirs BitriseDirs `yaml:"bitrise_dirs"`
@@ -14,8 +19,19 @@ type AgentConfig struct {
 }
 
 type BitriseDirs struct {
-	SourceDir     string `yaml:"BITRISE_SOURCE_DIR"`
-	DeployDir     string `yaml:"BITRISE_DEPLOY_DIR"`
+	// BitriseDataHomeDir is the root directory for all Bitrise data produced at runtime
+	BitriseDataHomeDir string `yaml:"BITRISE_DATA_HOME_DIR"`
+
+	// SourceDir is for source code checkouts.
+	// It might be outside of BitriseDataHomeDir if the user has configured it so
+	SourceDir string `yaml:"BITRISE_SOURCE_DIR"`
+
+	// DeployDir is for deployable artifacts.
+	// It might be outside of BitriseDataHomeDir if the user has configured it so
+	DeployDir string `yaml:"BITRISE_DEPLOY_DIR"`
+
+	// TestDeployDir is for deployable test result artifacts.
+	// It might be outside of BitriseDataHomeDir if the user has configured it so
 	TestDeployDir string `yaml:"BITRISE_TEST_DEPLOY_DIR"`
 }
 
@@ -30,7 +46,10 @@ type AgentHooks struct {
 	// Bitrise dirs defined in this config file are correctly expanded.
 	CleanupOnWorkflowEnd []string `yaml:"cleanup_on_workflow_end"`
 
+	// DoOnWorkflowStart is an optional executable to run when the workflow starts.
 	DoOnWorkflowStart string `yaml:"do_on_workflow_start"`
+
+	// DoOnWorkflowEnd is an optional executable to run when the workflow ends.
 	DoOnWorkflowEnd   string `yaml:"do_on_workflow_end"`
 }
 
@@ -46,49 +65,72 @@ func readAgentConfig(configFile string) (AgentConfig, error) {
 		return AgentConfig{}, err
 	}
 
+	expandedBitriseDataHomeDir, err := expandPath(config.BitriseDirs.BitriseDataHomeDir)
+	if err != nil {
+		return AgentConfig{}, fmt.Errorf("expand BITRISE_DATA_HOME_DIR value: %s", err)
+	}
+	config.BitriseDirs.BitriseDataHomeDir = expandedBitriseDataHomeDir
+
+	// BITRISE_SOURCE_DIR
+	if config.BitriseDirs.SourceDir == "" {
+		config.BitriseDirs.SourceDir = filepath.Join(config.BitriseDirs.BitriseDataHomeDir, defaultSourceDir)
+	}
 	expandedSourceDir, err := expandPath(config.BitriseDirs.SourceDir)
 	if err != nil {
 		return AgentConfig{}, fmt.Errorf("expand BITRISE_SOURCE_DIR value: %s", err)
 	}
 	config.BitriseDirs.SourceDir = expandedSourceDir
 
+	// BITRISE_DEPLOY_DIR
+	if config.BitriseDirs.DeployDir == "" {
+		config.BitriseDirs.DeployDir = filepath.Join(config.BitriseDirs.BitriseDataHomeDir, defaultDeployDir)
+	}
 	expandedDeployDir, err := expandPath(config.BitriseDirs.DeployDir)
 	if err != nil {
 		return AgentConfig{}, fmt.Errorf("expand BITRISE_DEPLOY_DIR value: %s", err)
 	}
 	config.BitriseDirs.DeployDir = expandedDeployDir
 
+	// BITRISE_TEST_DEPLOY_DIR
+	if config.BitriseDirs.TestDeployDir == "" {
+		config.BitriseDirs.TestDeployDir = filepath.Join(config.BitriseDirs.BitriseDataHomeDir, defaultTestDeployDir)
+	}
 	expandedTestDeployDir, err := expandPath(config.BitriseDirs.TestDeployDir)
 	if err != nil {
 		return AgentConfig{}, fmt.Errorf("expand BITRISE_TEST_DEPLOY_DIR value: %s", err)
 	}
 	config.BitriseDirs.TestDeployDir = expandedTestDeployDir
 
-	expandedDoOnWorkflowStart, err := expandPath(config.Hooks.DoOnWorkflowStart)
-	if err != nil {
-		return AgentConfig{}, fmt.Errorf("expand do_on_workflow_start value: %s", err)
+	// Hooks
+	if config.Hooks.DoOnWorkflowStart != "" {
+		expandedDoOnWorkflowStart, err := expandPath(config.Hooks.DoOnWorkflowStart)
+		if err != nil {
+			return AgentConfig{}, fmt.Errorf("expand do_on_workflow_start value: %s", err)
+		}
+		doOnWorkflowStartExists, err := pathutil.IsPathExists(expandedDoOnWorkflowStart)
+		if err != nil {
+			return AgentConfig{}, err
+		}
+		if !doOnWorkflowStartExists {
+			return AgentConfig{}, fmt.Errorf("do_on_workflow_start path does not exist: %s", expandedDoOnWorkflowStart)
+		}
+		config.Hooks.DoOnWorkflowStart = expandedDoOnWorkflowStart
 	}
-	doOnWorkflowStartExists, err := pathutil.IsPathExists(expandedDoOnWorkflowStart)
-	if err != nil {
-		return AgentConfig{}, err
-	}
-	if !doOnWorkflowStartExists {
-		return AgentConfig{}, fmt.Errorf("do_on_workflow_start path does not exist: %s", expandedDoOnWorkflowStart)
-	}
-	config.Hooks.DoOnWorkflowStart = expandedDoOnWorkflowStart
 
-	expandedDoOnWorkflowEnd, err := expandPath(config.Hooks.DoOnWorkflowEnd)
-	if err != nil {
-		return AgentConfig{}, fmt.Errorf("expand do_on_workflow_end value: %s", err)
+	if config.Hooks.DoOnWorkflowEnd != "" {
+		expandedDoOnWorkflowEnd, err := expandPath(config.Hooks.DoOnWorkflowEnd)
+		if err != nil {
+			return AgentConfig{}, fmt.Errorf("expand do_on_workflow_end value: %s", err)
+		}
+		doOnWorkflowEndExists, err := pathutil.IsPathExists(expandedDoOnWorkflowEnd)
+		if err != nil {
+			return AgentConfig{}, err
+		}
+		if !doOnWorkflowEndExists {
+			return AgentConfig{}, fmt.Errorf("do_on_workflow_end path does not exist: %s", expandedDoOnWorkflowEnd)
+		}
+		config.Hooks.DoOnWorkflowEnd = expandedDoOnWorkflowEnd
 	}
-	doOnWorkflowEndExists, err := pathutil.IsPathExists(expandedDoOnWorkflowEnd)
-	if err != nil {
-		return AgentConfig{}, err
-	}
-	if !doOnWorkflowEndExists {
-		return AgentConfig{}, fmt.Errorf("do_on_workflow_end path does not exist: %s", expandedDoOnWorkflowStart)
-	}
-	config.Hooks.DoOnWorkflowEnd = expandedDoOnWorkflowEnd
 
 	return config, nil
 }
--- configs/agent_config_test.go
+++ configs/agent_config_test.go
@@ -3,7 +3,6 @@ package configs
 import (
 	"io/ioutil"
 	"path/filepath"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,40 +17,55 @@ func TestReadAgentConfig(t *testing.T) {
 	require.NoError(t, err)
 
 	testCases := []struct {
-		name          string
+		name           string
 		configFile     string
 		expectedConfig AgentConfig
-		expectedErr   bool
+		expectedErr    bool
 	}{
 		{
-			name:        "Valid config file",
-			configFile:   "testdata/valid-agent-config.yml",
+			name:       "Full config file",
+			configFile: "testdata/full-agent-config.yml",
 			expectedConfig: AgentConfig{
-				BitriseDirs {
-					SourceDir:     "/opt/bitrise/workspace/ef7a9665e8b6408b",
-					DeployDir:     "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/artifacts",
-					TestDeployDir: "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/test_results",
+				BitriseDirs{
+					BitriseDataHomeDir: "/opt/bitrise",
+					SourceDir:          "/opt/bitrise/workspace/ef7a9665e8b6408b",
+					DeployDir:          "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/artifacts",
+					TestDeployDir:      "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/test_results",
 				},
-				AgentHooks {
-					CleanupOnWorkflowStart: []string { "$BITRISE_DEPLOY_DIR" },
-					CleanupOnWorkflowEnd: []string { "$BITRISE_TEST_DEPLOY_DIR" },
-					DoOnWorkflowStart: filepath.Join(tempDir, "cleanup.sh"),
-					DoOnWorkflowEnd: filepath.Join(tempDir, "cleanup.sh"),
+				AgentHooks{
+					CleanupOnWorkflowStart: []string{"$BITRISE_DEPLOY_DIR"},
+					CleanupOnWorkflowEnd:   []string{"$BITRISE_TEST_DEPLOY_DIR"},
+					DoOnWorkflowStart:      filepath.Join(tempDir, "cleanup.sh"),
+					DoOnWorkflowEnd:        filepath.Join(tempDir, "cleanup.sh"),
 				},
 			},
 			expectedErr: false,
 		},
 		{
-			name:        "Non-existent config file",
-			configFile:   "nonexistent",
-			expectedConfig: AgentConfig{},
-			expectedErr:   true,
+			name:       "Minimal config file",
+			configFile: "testdata/minimal-agent-config.yml",
+			expectedConfig: AgentConfig{
+				BitriseDirs{
+					BitriseDataHomeDir: "/opt/bitrise",
+					SourceDir:          "/opt/bitrise/workspace",
+					DeployDir:          "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/artifacts",
+					TestDeployDir:      "/opt/bitrise/ef7a9665e8b6408b/80b66786-d011-430f-9c68-00e9416a7325/test_results",
+				},
+				AgentHooks{},
+			},
+			expectedErr: false,
 		},
 		{
-			name:        "Config file with invalid YAML",
-			configFile:   "testdata",
+			name:           "Non-existent config file",
+			configFile:     "nonexistent",
 			expectedConfig: AgentConfig{},
-			expectedErr:   true,
+			expectedErr:    true,
+		},
+		{
+			name:           "Config file with invalid YAML",
+			configFile:     "testdata",
+			expectedConfig: AgentConfig{},
+			expectedErr:    true,
 		},
 	}
 
@@ -61,9 +75,10 @@ func TestReadAgentConfig(t *testing.T) {
 			if (err != nil) != tc.expectedErr {
 				t.Errorf("Unexpected error: %v", err)
 			}
-			if !reflect.DeepEqual(config, tc.expectedConfig) {
-				t.Errorf("Expected config: %v, but got: %v", tc.expectedConfig, config)
-			}
+			// if !reflect.DeepEqual(config, tc.expectedConfig) {
+			// 	t.Errorf("Expected config: %+v, but got: %+v", tc.expectedConfig, config)
+			// }
+			require.Equal(t, tc.expectedConfig, config)
 		})
 	}
 }
