12b0fb68a0b2f7974084ffb5051e2c50dade7ab3
LaszloP <7979773+lpusok@users.noreply.github.com>
2024-05-21 14:06:11 +0200

    Fix test

--- _tests/integration/offline_mode_test.go
+++ _tests/integration/offline_mode_test.go
@@ -4,15 +4,26 @@ import (
 	"testing"
 
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/stepman/stepman"
 	"github.com/stretchr/testify/require"
 )
 
-const offlineModeConfigPath = "offline_mode.yml"
+const (
+	offlineModeConfigPath = "offline_mode.yml"
+	defaultStepLibURI     = "https://github.com/bitrise-io/bitrise-steplib.git"
+)
 
 func preloadSteps(t *testing.T) {
+	// Clean steplib
+	route, found := stepman.ReadRoute(defaultStepLibURI)
+	require.True(t, found, "Failed to read route for Steplib: %s", defaultStepLibURI)
+
+	err := stepman.CleanupRoute(route)
+	require.NoError(t, err, "Failed to cleanup route: %s", route)
+
+	// Preload steps
 	cmd := command.New(binPath(), "steps", "preload", "--majors=1", "--minors=1", "--minors-since=0", "--patches-since=0")
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
-
 	require.NoError(t, err, "Preload failed, output: %s", out)
 }
 
