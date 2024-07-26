09a15b274ea81dcba8a35af35a3360b6ae56a633
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-07-26 10:04:04 +0200

    Add remote config module include e2e test case

--- _tests/integration/modular_config_test.go
+++ _tests/integration/modular_config_test.go
@@ -24,7 +24,12 @@ func Test_ModularConfig_Run(t *testing.T) {
 	cmd = command.New(binPath(), "workflows", "--id-only", "--config", configPth)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
-	require.Equal(t, "print_hello_bitrise print_hello_world", out)
+	require.Equal(t, "print_hello print_hello_bitrise print_hello_world", out)
+
+	cmd = command.New(binPath(), "run", "print_hello", "--config", configPth)
+	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+	require.NoError(t, err, out)
+	require.Contains(t, out, "Hello John Doe!")
 
 	cmd = command.New(binPath(), "run", "print_hello_bitrise", "--config", configPth)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
