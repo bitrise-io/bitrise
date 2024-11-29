2c6d339dbc87be05dfedf00b0407dd074d788e25
Balázs Hajagos <balazs.hajagos@bitrise.io>
2024-11-29 15:32:00 +0100

    Bump version to 2.24.4

--- version/build.go
+++ version/build.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION is the main CLI version number. It's defined at build time using -ldflags
-var VERSION = "2.24.3"
+var VERSION = "2.24.4"
 
 // BuildNumber is the CI build number that creates the release. It's defined at build time using -ldflags
 var BuildNumber = ""
