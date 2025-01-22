95ae8ded277e731ee38e0ba728c25ef52368635e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-01-22 16:02:53 +0100

    Remove unused params from integration test helper funcs

--- _tests/integration/helper.go
+++ _tests/integration/helper.go
@@ -14,9 +14,8 @@ func binPath() string {
 	return os.Getenv("INTEGRATION_TEST_BINARY_PATH")
 }
 
-func toBase64(t *testing.T, str string) string {
-	bytes := base64.StdEncoding.EncodeToString([]byte(str))
-	return string(bytes)
+func toBase64(str string) string {
+	return base64.StdEncoding.EncodeToString([]byte(str))
 }
 
 func toJSON(t *testing.T, stringStringMap map[string]interface{}) string {
@@ -25,7 +24,7 @@ func toJSON(t *testing.T, stringStringMap map[string]interface{}) string {
 	return string(bytes)
 }
 
-func withRunningTimeCheck(f func(), ms time.Duration) time.Duration {
+func withRunningTimeCheck(f func()) time.Duration {
 	start := time.Now()
 	f()
 	end := time.Now()
