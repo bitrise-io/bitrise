a2f2b5e702302658a0fd34057cce7166d3a41b65
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-01-22 16:06:22 +0100

    Update modular yaml integration tests with the new include.repository format

--- _tests/integration/modular_config_test.go
+++ _tests/integration/modular_config_test.go
@@ -14,32 +14,39 @@ import (
 func Test_ModularConfig(t *testing.T) {
 	configPth := "modular_config_main.yml"
 	deployDir := os.Getenv("BITRISE_DEPLOY_DIR")
+	currentRepositoryURLEnv := "BITRISE_CURRENT_REPOSITORY_URL=https://github.com/bitrise-io/bitrise.git"
 
 	cmd := command.New(binPath(), "merge", configPth, "-o", deployDir)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err := cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 
 	cmd = command.New(binPath(), "validate", "--config", configPth)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 	require.Equal(t, "Config is valid: \u001B[32;1mtrue\u001B[0m", out)
 
 	cmd = command.New(binPath(), "workflows", "--id-only", "--config", configPth)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 	require.Equal(t, "print_hello print_hello_bitrise print_hello_world", out)
 
 	cmd = command.New(binPath(), "run", "print_hello", "--config", configPth)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 	require.Contains(t, out, "Hello John Doe!")
 
 	cmd = command.New(binPath(), "run", "print_hello_bitrise", "--config", configPth)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 	require.Contains(t, out, "Hello Bitrise!")
 
 	cmd = command.New(binPath(), "run", "print_hello_world", "--config", configPth)
+	cmd.AppendEnvs(currentRepositoryURLEnv)
 	out, err = cmd.RunAndReturnTrimmedCombinedOutput()
 	require.NoError(t, err, out)
 	require.Contains(t, out, "Hello World!")
