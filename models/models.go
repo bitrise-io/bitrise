05da28d76fa6b26db78f1e21067ec85d208b7b5a
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-02-06 13:11:13 +0100

    Add title, summary and description to Step bundles

--- models/models.go
+++ models/models.go
@@ -25,12 +25,18 @@ const (
 )
 
 type StepBundleModel struct {
+	Title        string                              `json:"title,omitempty" yaml:"title,omitempty"`
+	Summary      string                              `json:"summary,omitempty" yaml:"summary,omitempty"`
+	Description  string                              `json:"description,omitempty" yaml:"description,omitempty"`
 	Inputs       []envmanModels.EnvironmentItemModel `json:"inputs,omitempty" yaml:"inputs,omitempty"`
 	Environments []envmanModels.EnvironmentItemModel `json:"envs,omitempty" yaml:"envs,omitempty"`
 	Steps        []StepListStepItemModel             `json:"steps,omitempty" yaml:"steps,omitempty"`
 }
 
 type StepBundleListItemModel struct {
+	Title        string                              `json:"title,omitempty" yaml:"title,omitempty"`
+	Summary      string                              `json:"summary,omitempty" yaml:"summary,omitempty"`
+	Description  string                              `json:"description,omitempty" yaml:"description,omitempty"`
 	Inputs       []envmanModels.EnvironmentItemModel `json:"inputs,omitempty" yaml:"inputs,omitempty"`
 	Environments []envmanModels.EnvironmentItemModel `json:"envs,omitempty" yaml:"envs,omitempty"`
 }
