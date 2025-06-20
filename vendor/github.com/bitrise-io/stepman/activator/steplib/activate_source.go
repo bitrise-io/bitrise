3696c7128804d8616cbc3fa1d665e24d4de59cc1
Olivér Falvai <ofalvai@gmail.com>
2025-06-20 15:39:49 +0200

    *

--- vendor/github.com/bitrise-io/stepman/activator/steplib/activate_source.go
+++ vendor/github.com/bitrise-io/stepman/activator/steplib/activate_source.go
@@ -25,29 +25,21 @@ func activateStepSource(
 	}
 
 	stepCacheDir := stepman.GetStepCacheDirPath(route, id, version)
-	if exist, err := pathutil.IsPathExists(stepCacheDir); err != nil {
+	stepCacheDirExists, err := pathutil.IsPathExists(stepCacheDir)
+	if err != nil {
 		return fmt.Errorf("failed to check if %s path exist: %s", stepCacheDir, err)
-	} else if exist {
-		if err := copyStep(stepCacheDir, destination); err != nil {
-			return fmt.Errorf("copy step failed: %s", err)
-		}
-		return nil
 	}
 
-	// version specific source cache not exists
-	if isOfflineMode {
-		availableVersions := ListCachedStepVersions(log, stepLib, stepLibURI, id)
-		versionList := "Other versions available in the local cache:"
-		for _, version := range availableVersions {
-			versionList = versionList + fmt.Sprintf("\n- %s", version)
+	if !stepCacheDirExists {
+		if isOfflineMode {
+			errMsg := collectOfflineAvailableStepVersions(stepLib, stepLibURI, id, version, log)
+			return fmt.Errorf("download step: %s", errMsg)
 		}
 
-		errMsg := fmt.Sprintf("version is not available in the local cache and $BITRISE_OFFLINE_MODE is set. %s", versionList)
-		return fmt.Errorf("download step: %s", errMsg)
-	}
-
-	if err := stepman.DownloadStep(stepLibURI, stepLib, id, version, step.Source.Commit, log); err != nil {
-		return fmt.Errorf("download failed: %s", err)
+		err := stepman.DownloadStep(stepLibURI, stepLib, id, version, step.Source.Commit, log)
+		if err != nil {
+			return fmt.Errorf("download failed: %s", err)
+		}
 	}
 
 	if err := copyStep(stepCacheDir, destination); err != nil {
@@ -74,4 +66,15 @@ func copyStep(src, dst string) error {
 		return fmt.Errorf("copy command failed: %s", err)
 	}
 	return nil
+}
+
+func collectOfflineAvailableStepVersions(stepLib models.StepCollectionModel, stepLibURI, id, version string, log stepman.Logger) string {
+	availableVersions := ListCachedStepVersions(log, stepLib, stepLibURI, id)
+	versionList := "Other versions available in the local cache:"
+	for _, version := range availableVersions {
+		versionList = versionList + fmt.Sprintf("\n- %s", version)
+	}
+
+	errMsg := fmt.Sprintf("version is not available in the local cache and $BITRISE_OFFLINE_MODE is set. %s", versionList)
+	return errMsg
 }
