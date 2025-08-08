c88a6bfd0ea42a949f3503479ac899e5a68f2e06
Olivér Falvai <oliver.falvai@bitrise.io>
2025-08-08 15:23:17 +0200

    Add more integration tests for supported tools

---
+++ _tests/integration/toolprovider/mise/install_golang_test.go
@@ -0,0 +1,48 @@
+//go:build linux_and_mac
+// +build linux_and_mac
+
+package mise
+
+import (
+	"testing"
+
+	"github.com/bitrise-io/bitrise/v2/toolprovider/mise"
+	"github.com/bitrise-io/bitrise/v2/toolprovider/provider"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAsdfInstallGolangVersion(t *testing.T) {
+	tests := []struct {
+		name               string
+		requestedVersion   string
+		resolutionStrategy provider.ResolutionStrategy
+		expectedVersion    string
+	}{
+		{"Install specific version", "1.23.4", provider.ResolutionStrategyStrict, "1.23.4"},
+		{"Install partial major.minor version", "1.22", provider.ResolutionStrategyLatestInstalled, "1.22.12"},
+		{"Install partial major.minor version, latest released", "1.22", provider.ResolutionStrategyLatestReleased, "1.22.12"},
+	}
+
+	for _, tt := range tests {
+		miseInstallDir := t.TempDir()
+		miseDataDir := t.TempDir()
+		miseProvider, err := mise.NewToolProvider(miseInstallDir, miseDataDir)
+		require.NoError(t, err)
+
+		err = miseProvider.Bootstrap()
+		require.NoError(t, err)
+
+		t.Run(tt.name, func(t *testing.T) {
+			request := provider.ToolRequest{
+				ToolName:           "golang",
+				UnparsedVersion:    tt.requestedVersion,
+				ResolutionStrategy: tt.resolutionStrategy,
+			}
+			result, err := miseProvider.InstallTool(request)
+			require.NoError(t, err)
+			require.Equal(t, provider.ToolID("golang"), result.ToolName)
+			require.Equal(t, tt.expectedVersion, result.ConcreteVersion)
+			require.False(t, result.IsAlreadyInstalled)
+		})
+	}
+}
