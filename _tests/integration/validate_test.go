9a0d3130f464c88890f5c94cbc17318e0a35ff67
Olivér Falvai <oliver.falvai@bitrise.io>
2025-08-08 15:23:17 +0200

    Add integration test for Mise config validation

--- _tests/integration/validate_test.go
+++ _tests/integration/validate_test.go
@@ -75,6 +75,27 @@ tool_config:
   extra_plugins:
     empty-url-tool: ""
 `
+const miseToolConfigYML = `format_version: 11
+default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+
+tools:
+  golang: "1.20.3"
+  nodejs: "20:latest"
+  ruby: "3.2:installed"
+
+tool_config:
+  provider: mise
+  extra_plugins:
+    flutter: "https://github.com/asdf-community/asdf-flutter.git"
+    custom-tool: "https://github.com/user/asdf-custom-tool.git"
+
+workflows:
+  test:
+    steps:
+    - script:
+        inputs:
+        - content: echo "hello"
+`
 const runtimeLimit = 1000 * time.Millisecond
 const runningTimeMsg = "test case too slow: %s is %s above limit"
 
@@ -269,6 +290,22 @@ func Test_InvalidToolConfigValidateTest(t *testing.T) {
 	require.Error(t, err, out)
 	require.Contains(t, out, "Config is valid: \x1b[31;1mfalse\x1b[0m")
 	require.Contains(t, out, "URL of extra plugin empty-url-tool is empty")
+	require.Equal(t, true, elapsed < runtimeLimit, runningTimeMsg, elapsed, elapsed-runtimeLimit)
+}
+
+func Test_ValidMiseToolConfigValidateTest(t *testing.T) {
+	tmpDir := t.TempDir()
+	configPth := filepath.Join(tmpDir, "bitrise.yml")
+	require.NoError(t, fileutil.WriteStringToFile(configPth, miseToolConfigYML))
+
+	var out string
+	var err error
+	elapsed := withRunningTimeCheck(func() {
+		cmd := command.New(binPath(), "validate", "-c", configPth)
+		out, err = cmd.RunAndReturnTrimmedCombinedOutput()
+	})
+	require.NoError(t, err, out)
+	require.Equal(t, "Config is valid: \x1b[32;1mtrue\x1b[0m", out)
 	require.Equal(t, true, elapsed < runtimeLimit, runningTimeMsg, elapsed, elapsed-runtimeLimit)
 }
 
