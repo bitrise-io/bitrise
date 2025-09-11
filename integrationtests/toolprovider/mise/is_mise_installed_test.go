d3e484a600a468ccbe2323fd6a31a2e74e1d8ff8
Marcell Vida <marcell.vida@bitrise.io>
2025-09-11 17:34:53 +0200

    Add integration test

---
+++ integrationtests/toolprovider/mise/is_mise_installed_test.go
@@ -0,0 +1,41 @@
+//go:build linux_and_mac
+// +build linux_and_mac
+
+package mise
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/bitrise-io/bitrise/v2/toolprovider/mise"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBootstrapSkipsInstallationWhenMiseAlreadyInstalled(t *testing.T) {
+	miseInstallDir := t.TempDir()
+	miseDataDir := t.TempDir()
+	miseProvider, err := mise.NewToolProvider(miseInstallDir, miseDataDir)
+	require.NoError(t, err)
+
+	// Should install
+	err = miseProvider.Bootstrap()
+	require.NoError(t, err)
+
+	// Exists and is executable test
+	misePath := filepath.Join(miseInstallDir, "bin", "mise")
+	info, err := os.Stat(misePath)
+	require.NoError(t, err)
+	assert.True(t, info.Mode().IsRegular())
+	assert.NotEqual(t, 0, info.Mode().Perm()&0111, "mise binary should be executable")
+
+	// Capture modification time, so we can verify it doesn't change on re-bootstrap
+	originalModTime := info.ModTime()
+	err = miseProvider.Bootstrap()
+	require.NoError(t, err)
+
+	info, err = os.Stat(misePath)
+	require.NoError(t, err)
+	assert.Equal(t, originalModTime, info.ModTime(), "mise binary should not have been reinstalled")
+}
