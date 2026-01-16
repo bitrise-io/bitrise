f58aa3aef65c70c50583e705cda2520c8d11f3b4
Ben Boral <ben.boral@bitrise.io>
2026-01-16 16:15:00 -0600

    update test limits

--- configmerge/configmerge_test.go
+++ configmerge/configmerge_test.go
@@ -46,7 +46,7 @@ include:
 			wantErr:       "circular reference detected: bitrise.yml -> module_1.yml -> module_2.yml -> module_1.yml",
 		},
 		{
-			name: "Max 10 include items are allowed",
+			name: "Max 35 include items are allowed",
 			configReader: mockConfigReader{
 				fileSystemFiles: map[string][]byte{
 					"bitrise.yml": []byte(fmt.Sprintf(`format_version: "15"
@@ -57,10 +57,10 @@ include:
 				},
 			},
 			mainConfigPth: "bitrise.yml",
-			wantErr:       "max include count (10) exceeded",
+			wantErr:       "max include count (35) exceeded",
 		},
 		{
-			name: "Max 20 config files are allowed",
+			name: "Max 35 config files are allowed",
 			configReader: mockConfigReader{
 				fileSystemFiles: map[string][]byte{
 					"bitrise.yml": []byte(fmt.Sprintf(`format_version: "15"
@@ -74,7 +74,7 @@ include:
 				},
 			},
 			mainConfigPth: "bitrise.yml",
-			wantErr:       "max file count (20) exceeded",
+			wantErr:       "max file count (35) exceeded",
 		},
 		{
 			name: "Max include depth is 5",
