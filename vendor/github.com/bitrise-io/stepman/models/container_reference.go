f2bad1fb0447147655af2c4f03717ccef978262f
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2026-01-26 10:38:56 +0100

    Update stepman to container-support-for-steps branch

---
+++ vendor/github.com/bitrise-io/stepman/models/container_reference.go
@@ -0,0 +1,83 @@
+package models
+
+import "fmt"
+
+// ContainerReference is a reference to a container. The value is either the container id, or a map with the container id and additional configuration.
+//
+//	service_containers:
+//	- redis
+//	- postgres:
+//	    recreate: true
+type ContainerReference any
+
+type ContainerConfig struct {
+	ContainerID string
+	Recreate    bool
+}
+
+func GetContainerConfig(ref ContainerReference) (*ContainerConfig, error) {
+	if ref == nil {
+		return nil, nil
+	}
+
+	switch ref.(type) {
+	case string:
+		ctrStr := ref.(string)
+		return getContainerConfigFromString(ctrStr), nil
+	case map[any]any:
+		ctrMap := ref.(map[any]any)
+		return getContainerConfigFromMap(ctrMap)
+	default:
+		return nil, fmt.Errorf("invalid container config type: %T", ref)
+	}
+}
+
+func getContainerConfigFromString(ctrStr string) *ContainerConfig {
+	return &ContainerConfig{
+		ContainerID: ctrStr,
+		Recreate:    false,
+	}
+}
+
+func getContainerConfigFromMap(ctrMap map[any]any) (*ContainerConfig, error) {
+	if len(ctrMap) != 1 {
+		return nil, fmt.Errorf("invalid container config map length: %d", len(ctrMap))
+	}
+
+	var id string
+	var recreate bool
+
+	for k, v := range ctrMap {
+		var ok bool
+		id, ok = k.(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid container config ID type: %T", k)
+		}
+
+		ctrCfg, ok := v.(map[any]any)
+		if !ok {
+			return nil, fmt.Errorf("invalid container config value type: %T", v)
+		}
+		if len(ctrCfg) > 1 {
+			return nil, fmt.Errorf("invalid container config value map length: %d", len(ctrCfg))
+		}
+
+		if len(ctrCfg) == 1 {
+			recreateVal, ok := ctrCfg["recreate"]
+			if !ok {
+				return nil, fmt.Errorf("missing recreate key in container config")
+			}
+
+			recreate, ok = recreateVal.(bool)
+			if !ok {
+				return nil, fmt.Errorf("invalid recreate value type: %T", recreateVal)
+			}
+		}
+		break
+	}
+
+	return &ContainerConfig{
+		ContainerID: id,
+		Recreate:    recreate,
+	}, nil
+}
