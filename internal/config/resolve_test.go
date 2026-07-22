91403a766ff0f10250668e83a9fcd39772b1dc0c
Márton Molnár <marton.molnar@bitrise.io>
2026-07-22 14:28:12 +0200

    merge fix

--- internal/config/resolve_test.go
+++ internal/config/resolve_test.go
@@ -74,6 +74,7 @@ func TestResolve_LastCLIUpdateCheckPrecedence(t *testing.T) {
 
 func TestResolve_LastPluginUpdateChecksPrecedence(t *testing.T) {
 	legacy := Config{LastPluginUpdateChecks: map[string]time.Time{"legacy-plugin": time.Now()}}
+	dir := Config{LastPluginUpdateChecks: map[string]time.Time{"dir-plugin": time.Now()}}
 	global := Config{LastPluginUpdateChecks: map[string]time.Time{"global-plugin": time.Now()}}
 
 	r := Resolve(legacy, Config{}, Config{})
