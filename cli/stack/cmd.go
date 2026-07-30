853983cdffc32fdcd4ffa764d3cdffd0fa64c20d
Márton Molnár <marton.molnar@bitrise.io>
2026-07-30 09:10:29 +0200

    review fixes

--- cli/stack/cmd.go
+++ cli/stack/cmd.go
@@ -9,14 +9,8 @@ import (
 func NewCmd() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "stack",
-		Short: "List available stacks",
-		Long: `List the build stacks available to you.
-
-Running "bitrise stack" with no subcommand lists stacks.`,
-		Example: `  bitrise stack list
-  bitrise stack list --format json
-  bitrise stack list --workspace WORKSPACE_ID`,
-		RunE: cmdutil.DelegateToList,
+		Short: "Manage build stacks.",
+		RunE:  cmdutil.RequireKnownSubcommand,
 	}
 	c.AddCommand(NewListCommand())
 	return c
