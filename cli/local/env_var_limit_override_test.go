c04c8d86f354a568a577026eba7fea66e2dd1a62
Márton Molnár <marton.molnar@bitrise.io>
2026-09-14 15:36:10 +0200

    fix main merge

--- cli/local/env_var_limit_override_test.go
+++ cli/local/env_var_limit_override_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/bitrise-io/bitrise/v2/analytics/analyticstest"
 	"github.com/bitrise-io/envman/v2/envman"
 	envmanModels "github.com/bitrise-io/envman/v2/models"
 	"github.com/stretchr/testify/require"
@@ -11,7 +12,7 @@ import (
 
 func TestApplyEnvVarLimitOverrides(t *testing.T) {
 	//nolint:exhaustruct // the override logic reads none of the config
-	runner := NewWorkflowRunner(RunConfig{}, nil, noOpTracker{})
+	runner := NewWorkflowRunner(RunConfig{}, nil, analyticstest.NoOpTracker{})
 	key := envman.EnvListBytesLimitInKBEnvKey
 
 	tests := []struct {
